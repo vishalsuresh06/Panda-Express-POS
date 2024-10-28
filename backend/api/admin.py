a1ae9e8a823a6c5c13a00952b4d91de359b92dac
rpomullan <rpomullan@gmail.com>
2024-10-28 12:50:34 -0500

    Moved models over to new file setup
    Also made it easier to create inventory food item connections on the admin page

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+
+# These classes allow for easier creation of orders on the admin page
+class OrderItemInline(admin.TabularInline):
+    model = OrderItem
+    extra = 1
+
+class OrderAdmin(admin.ModelAdmin):
+    inlines = [OrderItemInline]
+
+admin.site.register(Order, OrderAdmin)
+admin.site.register(OrderItem)
+admin.site.register(OrderItemType)
+admin.site.register(Employee)
+
+
+class FoodInventoryInline(admin.TabularInline):
+    model = FoodInventoryQuantity
+    extra = 1
+    fk_name = "food_item"
+
+class FoodItemAdmin(admin.ModelAdmin):
+    inlines = [FoodInventoryInline]
+
+admin.site.register(FoodItem, FoodItemAdmin)
+admin.site.register(InventoryItem)
+admin.site.register(FoodInventoryQuantity)
+
