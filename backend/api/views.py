ead7316d42a1f37e9da20a2837c20d07dea99cd1
solarn26 <144829748+solarn26@users.noreply.github.com>
2024-10-30 21:24:19 +0000

    Connect manager page to backend

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,23 +1,54 @@
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
-
 from django.http import JsonResponse
 from django.core import serializers
-from .serializers import OrderSerializer, OrderItemSerializer, FoodItemSerializer, OrderItemTypeSerializer
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .serializers import *
 from .models import *
 
 @api_view(['GET'])
 def helloworld(request):
     return Response("Hello, World!")
 
-@api_view(['GET'])
-def getEmployees(request):
-    rawData = Employee.objects.all()
-    serializedData = serializers.serialize('json', rawData)
-    return JsonResponse(serializedData, safe=False)
+class EmployeeView(APIView):
+    def get(self, request):
+        rawData = Employee.objects.all()
+        serializer = EmployeeSerializer(rawData, many=True)
+        return JsonResponse(serializer.data, safe=False)
+    def post(self, request):
+        print(request.data)
+        action = request.data['action']
+        data = request.data['data']
+        
+        if action == "add":
+            serializer = EmployeeSerializer(data=data)
+            if serializer.is_valid():
+                serializer.save()
+
+                return JsonResponse({"success": True}, status=status.HTTP_201_CREATED)
+
+            return JsonResponse({"success": True}, status=status.HTTP_201_CREATED)
+        elif action == "delete":
+            print("Test")
+            employee = Employee.objects.get(id=data)
+            employee.delete()
+
+            return JsonResponse({"success": True}, status=status.HTTP_201_CREATED)
+        else:
+            employee = Employee.objects.get(id=data['id'])
+            serializer = EmployeeSerializer(employee, data=data, partial=True)
+
+            if serializer.is_valid():
+                serializer.save()
+                return JsonResponse({"success": True}, status=status.HTTP_201_CREATED)
+            else:
+                print("Invalid input")
+
+        return JsonResponse({"success": False}, status=status.HTTP_406_NOT_ACCEPTABLE)
 
 @api_view(['GET'])
 def getOrders(request):
     rawData = Order.objects.all()
-    serializer = OrderSerializer(rawData, many=True);
-    return JsonResponse(serializer.data, safe=False);
+    serializer = OrderSerializer(rawData, many=True)
+    return JsonResponse(serializer.data, safe=False)
