312f7f5066744554e3048a9ee4930328f6d8f804
phoenyliam <phoenyxonfire@tamu.edu>
2024-11-13 15:46:22 -0600

    getting stuff working

--- src/pages/cashiers/index.jsx
+++ src/pages/cashiers/index.jsx
@@ -1,6 +1,6 @@
-import NavBar from "./NavBar";
-import CheckoutView from "./Checkout";
-import OrderView from "./OrderView";
+import NavBar from "./cashierpages/NavBar";
+import CheckoutView from "./cashierpages/Checkout";
+import OrderView from "./cashierpages/OrderView";
 
 function Cashier() {
   return (
